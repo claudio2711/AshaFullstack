8c95c661a49c81a9dbef6dc15f9df58b1826645e
Claudio Di Salvo <claudio27-11@hotmail.com>
2025-07-14 19:29:50 +0200

    configurazione database

---
+++ back/src/controllers/songController.ts
@@ -0,0 +1,14 @@
+import {Request, Response} from "express"
+
+const addSong = async (req: Request, res: Response) =>
+{
+
+}
+
+const listSong = async (req: Request, res: Response) =>
+{
+    
+}
+
+
+export {addSong, listSong}
